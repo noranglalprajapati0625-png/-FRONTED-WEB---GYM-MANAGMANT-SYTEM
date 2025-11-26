603c2b782080efad00903bb23e66a917ddf1b077
noranglalprajapati0625-png <noranglalprajapati0625@gmail.com>
2025-11-25 22:14:50 -0800

    Add files via upload

---
+++ gym.js
@@ -0,0 +1,41 @@
+function home(){
+    document.getElementById("homes").style.color=" rgb(216, 202, 11)";
+    document.getElementById("plans").style.color=" white";
+     document.getElementById("contacts").style.color=" white";
+      document.getElementById("blogs").style.color=" white";
+       document.getElementById("programs").style.color=" white";
+}
+function program(){
+    document.getElementById("homes").style.color=" white";
+    document.getElementById("programs").style.color="  rgb(216, 202, 11)";
+     document.getElementById("contacts").style.color=" white";
+      document.getElementById("blogs").style.color=" white";
+       document.getElementById("plans").style.color=" white";
+}
+function plan(){
+    document.getElementById("plans").style.color=" rgb(216, 202, 11)";
+    document.getElementById("homes").style.color=" white";
+     document.getElementById("contacts").style.color=" white";
+      document.getElementById("blogs").style.color=" white";
+       document.getElementById("programs").style.color=" white";
+}
+
+function blog(){
+    document.getElementById("blogs").style.color=" rgb(216, 202, 11)";
+    document.getElementById("plans").style.color=" white";
+     document.getElementById("homes").style.color=" white";
+      document.getElementById("contacts").style.color=" white";
+       document.getElementById("programs").style.color=" white";
+}
+function contact(){
+    document.getElementById("contacts").style.color=" rgb(216, 202, 11)";
+    document.getElementById("plans").style.color=" white";
+     document.getElementById("homes").style.color=" white";
+      document.getElementById("blogs").style.color=" white";
+       document.getElementById("programs").style.color=" white";
+}
+
+
+ 
+
+
